4dfbc2830a27360e7e3c0d4df35a8b919c00b0fc
vearne <asdwoshiaotian@gmail.com>
2024-01-04 16:10:09 +0800

    add prom in OperatorDirs

--- tools/go-agent/instrument/plugins/rewrite/context.go
+++ tools/go-agent/instrument/plugins/rewrite/context.go
@@ -39,7 +39,7 @@ var GenerateCommonPrefix = "skywalking_"
 
 var GenerateMethodPrefix = GenerateCommonPrefix + "enhance_"
 var GenerateVarPrefix = GenerateCommonPrefix + "var_"
-var OperatorDirs = []string{"operator", "log", "tracing", "tools", "metrics"}
+var OperatorDirs = []string{"operator", "log", "tracing", "tools", "metrics", "prom"}
 
 var OperatePrefix = GenerateCommonPrefix + "operator"
 var TypePrefix = OperatePrefix + "Type"
